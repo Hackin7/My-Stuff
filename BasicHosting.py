5f0701434f957b0086df1ca6e71a2cd2f957adea
Terence Chan Zun Mun <zunmun@gmail.com>
2021-03-11 22:49:44 +0800

    Overhaul
    Shift from jekyll to a custom system

---
+++ BasicHosting.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, send_from_directory
+
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='', static_folder="./")
+
+@app.route('/')
+def root():
+    return send_from_directory('./', 'index.html')
+
+app.run()
